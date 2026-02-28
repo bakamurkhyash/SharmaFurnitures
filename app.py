1e5b9aaeefa15e17c02b86cf050caf4b8d9ad54e
bakamurkhyash <bakamurkhyash@gmail.com>
2026-02-28 14:21:04 +0530

    project done

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
 from flask import Flask, render_template
+import cloudinary
+import cloudinary.api
+from cloudinary.utils import cloudinary_url
 
 app = Flask(__name__)
+
+cloudinary.config(
+    cloud_name="dlkjvnxpu",
+    api_key="288393286726996",
+    api_secret="i3pm1Q9GRnMwY-HAh62mbj1caz0"
+)
+
+result = cloudinary.api.resources(type = "upload", max_results = 77)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+@app.route('/gallery')
+def gallery():
+    return result
+
 @app.route('/portfolio')
 def portfolio():
-    return render_template('portfolio.html')
+    return render_template('portfolio.html', images=result.get('resources', []))
 
 @app.route('/calculator')
 def calculator():
